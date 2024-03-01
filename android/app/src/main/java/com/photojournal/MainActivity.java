76b10458274d53f24ac50b88143b1bc8725a25ba
vansh soni <vansh.s7714@gmail.com>
2024-03-01 16:13:44 +0530

    push

--- android/app/src/main/java/com/photojournal/MainActivity.java
+++ android/app/src/main/java/com/photojournal/MainActivity.java
@@ -8,6 +8,7 @@ import com.facebook.react.ReactRootView;
 import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
 import expo.modules.splashscreen.singletons.SplashScreen;
+
 import expo.modules.splashscreen.SplashScreenImageResizeMode;
 
 public class MainActivity extends ReactActivity {
